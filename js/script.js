cee7bc22d3cdfd13e1c602b86ec72de981afb775
koichi-inoue <inoue.ko@gmail.com>
2020-06-15 17:11:04 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready( function(){
+
+	$(".fade").fadeTo(0,0.2); // 初期状態 20%
+
+	$(".fade").hover(
+		function(){
+			$(this).fadeTo("slow", 1.0); // マウスが載ったら100%までフェードする
+		},
+		function(){
+			$(this).fadeTo("slow", 0.2); // マウスが離れたら20%に戻す
+		}
+	);
+});
